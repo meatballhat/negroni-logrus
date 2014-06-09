25f52df21000e8ae4e822f70db051d67bee82148
Dan Buch <dan@meatballhat.com>
2014-06-09 17:23:49 -0400

    Adding a smidge of customization

--- middleware.go
+++ middleware.go
@@ -16,9 +16,16 @@ type Middleware struct {
 
 // NewMiddleware returns a new *Middleware, yay!
 func NewMiddleware() *Middleware {
-	return &Middleware{
-		Logger: logrus.New(),
-	}
+	return NewCustomMiddleware(logrus.Info, &logrus.TextFormatter{})
+}
+
+// NewCustomMiddleware builds a *Middleware with the given level and formatter
+func NewCustomMiddleware(level logrus.Level, formatter logrus.Formatter) *Middleware {
+	log := logrus.New()
+	log.Level = level
+	log.Formatter = formatter
+
+	return &Middleware{Logger: log}
 }
 
 func (l *Middleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
