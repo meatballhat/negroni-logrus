5897de2ffaefff5f786e81780bcfcf9a772f277e
Dan Buch <d.buch@modcloth.com>
2014-06-27 16:50:34 -0400

    Allowing for custom latency metric key name

--- middleware.go
+++ middleware.go
@@ -1,6 +1,7 @@
 package negronilogrus
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -12,20 +13,22 @@ import (
 type Middleware struct {
 	// Logger is the log.Logger instance used to log messages with the Logger middleware
 	Logger *logrus.Logger
+	// Name is the name of the application as recorded in latency metrics
+	Name string
 }
 
 // NewMiddleware returns a new *Middleware, yay!
 func NewMiddleware() *Middleware {
-	return NewCustomMiddleware(logrus.Info, &logrus.TextFormatter{})
+	return NewCustomMiddleware(logrus.Info, &logrus.TextFormatter{}, "web")
 }
 
 // NewCustomMiddleware builds a *Middleware with the given level and formatter
-func NewCustomMiddleware(level logrus.Level, formatter logrus.Formatter) *Middleware {
+func NewCustomMiddleware(level logrus.Level, formatter logrus.Formatter, name string) *Middleware {
 	log := logrus.New()
 	log.Level = level
 	log.Formatter = formatter
 
-	return &Middleware{Logger: log}
+	return &Middleware{Logger: log, Name: name}
 }
 
 func (l *Middleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
@@ -41,12 +44,12 @@ func (l *Middleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, next htt
 	latency := time.Since(start)
 	res := rw.(negroni.ResponseWriter)
 	l.Logger.WithFields(logrus.Fields{
-		"status":              res.Status(),
-		"method":              r.Method,
-		"request":             r.RequestURI,
-		"remote":              r.RemoteAddr,
-		"text_status":         http.StatusText(res.Status()),
-		"took":                latency,
-		"measure#web.latency": latency,
+		"status":      res.Status(),
+		"method":      r.Method,
+		"request":     r.RequestURI,
+		"remote":      r.RemoteAddr,
+		"text_status": http.StatusText(res.Status()),
+		"took":        latency,
+		fmt.Sprintf("measure#%s.latency", l.Name): latency,
 	}).Info("completed handling request")
 }
